f902d72ffa2f63d2548922856eb69f4f2582db16
Kyros70 <zaedmasri000999@gmail.com>
2026-07-05 01:30:04 +0300

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -153,6 +153,8 @@ TW_BRIGHTNESS_PATH := "/sys/class/backlight/sprd_backlight/brightness"
 TW_MAX_BRIGHTNESS := 4095
 TW_CUSTOM_CPU_TEMP_PATH := "/sys/class/thermal/thermal_zone0/temp"
 TW_DEFAULT_BRIGHTNESS := 4095
+TW_LOAD_VENDOR_DLKM_MODULES := "adaptive-ts.ko focaltech_ft8756_spi_ts.ko"
+TW_LOAD_VENDOR_BOOT_MODULES := true
 TW_CUSTOM_BATTERY_PATH := /sys/devices/platform/charger-manager/power_supply/battery
 
 # INCLUDE TWRP CONFIG
